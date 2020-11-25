b93b2a0a9887376e632c1dd5f1f4574580f45307
Clement <cdreptin@gmail.com>
2020-11-25 19:32:13 +0100

    First version

---
+++ ModuleLoader/src/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace ModuleLoader
+{
+	class Program
+	{
+		static ModuleLoader ModuleLoader = new ModuleLoader();
+		delegate void ModuleCallBack(string moduleName);
+
+		static void Main(string[] args)
+		{
+			if (args.Length == 0)
+			{
+				ModuleLoader.ShowHelp();
+				return;
+			}
+
+			if (args.Length > 2)
+			{
+				ModuleLoader.Print("error", "Maximum number of arguments exceeded. ModuleLoader -h to see the usage.");
+				return;
+			}
+
+			string firstArg = (args.Length > 0 && args[0] != null) ? args[0] : null;
+			string secondArg = (args.Length > 1 && args[1] != null) ? args[1] : null;
+
+			if (firstArg.StartsWith("-"))
+			{
+				switch (firstArg)
+				{
+					case "-h":
+						ModuleLoader.ShowHelp();
+						break;
+					case "-l":
+						if (secondArg != null)
+							ModuleAction(Load, secondArg);
+						else
+							ModuleLoader.Print("error", "You need to specify an absolute module path. ModuleLoader -h to see the usage.");
+						break;
+					case "-u":
+						if (secondArg != null)
+							ModuleAction(Unload, secondArg);
+						else
+							ModuleLoader.Print("error", "You need to specify an absolute module path. ModuleLoader -h to see the usage.");
+						break;
+					default:
+						ModuleLoader.Print("error", firstArg + " is not a valid argument. ModuleLoader -h to see the usage.");
+						break;
+				}
+			}
+			else
+			{
+				if (secondArg != null)
+					ModuleLoader.Print("error", "You can not specify argument after the module path. ModuleLoader -h to see the usage.");
+				else
+					ModuleAction(UnloadThenLoad, firstArg);
+			}
+		}
+
+		static void Load(string moduleName)
+		{
+			ModuleLoader.LoadModule(moduleName);
+		}
+
+		static void Unload(string moduleName)
+		{
+			ModuleLoader.UnloadModule(moduleName);
+		}
+
+		static void UnloadThenLoad(string moduleName)
+		{
+			if (ModuleLoader.IsModuleLoaded(moduleName))
+				ModuleLoader.UnloadModule(moduleName);
+
+			ModuleLoader.LoadModule(moduleName);
+		}
+
+		static void ModuleAction(ModuleCallBack moduleCallBack, string moduleName)
+		{
+			if (ModuleLoader.Connect())
+			{
+				moduleCallBack(moduleName);
+
+				ModuleLoader.Disconnect();
+			}
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ModuleLoader
 ModuleLoader is a command line tool to load/unload modules from an RGH/Jtag/Devkit Xbox 360 without having to reboot.
+
+## Requirements
+You must have the Xbox 360 SDK (XDK) installed. If you have another version than 2.0.21256.0 you will need to replace the dll files from the `lib` folder with the ones from your SDK (located at `\path\to\XDK\bin\win32\`).
+
+## Documentation
+- `-h`: Show help.
+- `<module_path>`: If `<module_path>` is already loaded, it will be unloaded then loaded back, otherwise it will just be loaded.
+- `-l <module_path>`: Load the module located at `<module_path>` (absolute path).
+- `-u <module_path>`: Unload the module located at `<module_path>` (absolute path).
