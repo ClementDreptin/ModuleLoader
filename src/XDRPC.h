6a4269f34dac13f62ad5d4b83ad5b73c022f8388
Clément Dreptin <cdreptin@gmail.com>
2023-04-02 00:18:50 +0200

    Simplified .gitignore

--- src/XDRPC.h
+++ src/XDRPC.h
@@ -3,21 +3,17 @@
 #include <Windows.h>
 #include <stdint.h>
 
-enum _XdrpcArgType
+typedef enum _XdrpcArgType
 {
     XdrpcArgType_Integer,
     XdrpcArgType_String,
-};
+} XdrpcArgType;
 
-typedef enum _XdrpcArgType XdrpcArgType;
-
-struct _XdrpcArgInfo
+typedef struct _XdrpcArgInfo
 {
     const void *pData;
     XdrpcArgType Type;
-};
-
-typedef struct _XdrpcArgInfo XdrpcArgInfo;
+} XdrpcArgInfo;
 
 // Call the function at ordinal in moduleName and pass it numberOfArgs arguments coming from args.
 HRESULT XdrpcCall(const char *moduleName, uint32_t ordinal, XdrpcArgInfo *args, size_t numberOfArgs, uint64_t *pReturnValue);
